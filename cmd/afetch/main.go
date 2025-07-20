fb6f4895e5035600c9d140084580815d0ac16da6
TWolfis <Thomas.wolfis@gmail.com>
2025-07-20 12:30:12 +0200

    added yaml parsing

--- cmd/afetch/main.go
+++ cmd/afetch/main.go
@@ -11,64 +11,73 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// Custom usage function for better help formatting
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), `afetch - NASA Astronomy Picture of the Day (APOD) Fetcher
+
+USAGE:
+    %s [FLAGS]
+
+FLAGS:
+`, os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintf(flag.CommandLine.Output(), `
+EXAMPLES:
+    %s -d 2023-07-20                    # Get APOD for a specific date
+    %s -sd 2023-07-01 -ed 2023-07-07    # Get APOD range
+    %s -d 2023-07-20 -dl -hd            # Download HD image for specific date
+    %s --save-yaml --yaml-file config.yaml  # Save current options to YAML
+    %s --read-yaml --yaml-file config.yaml  # Read options from YAML
+
+ENVIRONMENT VARIABLES:
+    DB2_PASSWORD               DB2 database password (alternative to --db2pass flag)
+
+`, os.Args[0], os.Args[0], os.Args[0], os.Args[0], os.Args[0])
+	}
+
+	// Define all flags
+	flag.BoolVar(&opts.Download, "dl", false, "download image")
+	flag.StringVar(&opts.DstFile, "df", "", "destination file for downloaded image")
+	flag.BoolVar(&opts.Hdurl, "hd", false, "use hd image url")
+	flag.StringVar(&opts.Date, "d", "", "date for APOD in format YYYY-MM-DD")
+	flag.StringVar(&opts.Startdate, "sd", "", "start date for APOD range in format YYYY-MM-DD")
+	flag.StringVar(&opts.Enddate, "ed", "", "end date for APOD range in format YYYY-MM-DD")
+
+	flag.BoolVar(&opts.Db2, "db2", false, "save to DB2 database")
+	flag.StringVar(&opts.Db2User, "db2user", "db2inst1", "DB2 username")
+	flag.StringVar(&opts.Db2Password, "db2pass", "", "DB2 password")
+	flag.StringVar(&opts.Db2Database, "db2db", "nasa", "DB2 database name")
+	flag.StringVar(&opts.Db2Table, "db2table", "apod", "DB2 table name")
+
+	flag.BoolVar(&saveToYaml, "save-yaml", false, "save options to YAML file")
+	flag.BoolVar(&readFromYaml, "read-yaml", false, "read options from YAML file")
+	flag.StringVar(&yamlFile, "yaml-file", "apod_options.yaml", "YAML file path for saving/reading options")
+	flag.BoolVar(&help, "h", false, "show help message")
+}
+
+// Global variables for flags
+var (
+	opts         Options
+	saveToYaml   bool
+	readFromYaml bool
+	yamlFile     string
+	help         bool
+)
+
 type Options struct {
-	date      string `yaml:"date"`
-	startdate string `yaml:"startdate"`
-	enddate   string `yaml:"enddate"`
-	download  bool   `yaml:"download"`
-	dstFile   string `yaml:"dstFile"`
-	hdurl     bool   `yaml:"hdurl"`
+	Date      string `yaml:"date"`
+	Startdate string `yaml:"startdate"`
+	Enddate   string `yaml:"enddate"`
+	Download  bool   `yaml:"download"`
+	DstFile   string `yaml:"dstFile"`
+	Hdurl     bool   `yaml:"hdurl"`
 
-	db2         bool   `yaml:"db2"`
-	db2Password string `yaml:"db2Password"`
-	db2Database string `yaml:"db2Database"`
-	db2Table    string `yaml:"db2Table"`
-	db2User     string `yaml:"db2User"`
-}
-
-func SetOptions() (*Options, error) {
-	opts := &Options{}
-
-	flag.StringVar(&opts.date, "d", "", "date for APOD in format YYYY-MM-DD")
-	flag.StringVar(&opts.startdate, "sd", "", "start date for APOD range in format YYYY-MM-DD")
-	flag.StringVar(&opts.enddate, "ed", "", "end date for APOD range in format YYYY-MM-DD")
-	flag.BoolVar(&opts.download, "dl", false, "download image")
-	flag.StringVar(&opts.dstFile, "df", "", "destination file for downloaded image")
-	flag.BoolVar(&opts.hdurl, "hd", false, "use hd image url")
-
-	flag.BoolVar(&opts.db2, "db2", false, "save to DB2 database")
-	flag.StringVar(&opts.db2User, "db2user", "db2inst1", "DB2 username")
-	flag.StringVar(&opts.db2Password, "db2pass", "", "DB2 password")
-	flag.StringVar(&opts.db2Database, "db2db", "nasa", "DB2 database name")
-	flag.StringVar(&opts.db2Table, "db2table", "apod", "DB2 table name")
-
-	flag.Parse()
-
-	return opts, nil
-}
-
-func SetOptionsWithYamlFlags() (*Options, *bool, *bool, *string, error) {
-	opts := &Options{}
-	saveToYaml := flag.Bool("save-yaml", false, "save options to YAML file")
-	readFromYaml := flag.Bool("read-yaml", false, "read options from YAML file")
-	yamlFile := flag.String("yaml-file", "apod_options.yaml", "YAML file path for saving/reading options")
-
-	flag.StringVar(&opts.date, "d", "", "date for APOD in format YYYY-MM-DD")
-	flag.StringVar(&opts.startdate, "sd", "", "start date for APOD range in format YYYY-MM-DD")
-	flag.StringVar(&opts.enddate, "ed", "", "end date for APOD range in format YYYY-MM-DD")
-	flag.BoolVar(&opts.download, "dl", false, "download image")
-	flag.StringVar(&opts.dstFile, "df", "", "destination file for downloaded image")
-	flag.BoolVar(&opts.hdurl, "hd", false, "use hd image url")
-
-	flag.BoolVar(&opts.db2, "db2", false, "save to DB2 database")
-	flag.StringVar(&opts.db2User, "db2user", "db2inst1", "DB2 username")
-	flag.StringVar(&opts.db2Password, "db2pass", "", "DB2 password")
-	flag.StringVar(&opts.db2Database, "db2db", "nasa", "DB2 database name")
-	flag.StringVar(&opts.db2Table, "db2table", "apod", "DB2 table name")
-
-	flag.Parse()
-
-	return opts, saveToYaml, readFromYaml, yamlFile, nil
+	Db2         bool   `yaml:"db2"`
+	Db2Password string `yaml:"db2Password"`
+	Db2Database string `yaml:"db2Database"`
+	Db2Table    string `yaml:"db2Table"`
+	Db2User     string `yaml:"db2User"`
 }
 
 func SetOptionsFromFile(filePath string) (*Options, error) {
@@ -148,10 +157,10 @@ func SaveImage(a *goapod.Apod, dstFile string, hdurl bool) error {
 }
 
 func SaveToDb2(a *goapod.Apod, opts *Options) error {
-	if opts.db2Password == "" {
-		opts.db2Password = os.Getenv("DB2_PASSWORD")
+	if opts.Db2Password == "" {
+		opts.Db2Password = os.Getenv("DB2_PASSWORD")
 
-		if opts.db2Password == "" {
+		if opts.Db2Password == "" {
 			fmt.Println("DB2 password not set. Please set the DB2_PASSWORD environment variable or flag.")
 			return fmt.Errorf("DB2 password not set")
 		}
@@ -159,11 +168,11 @@ func SaveToDb2(a *goapod.Apod, opts *Options) error {
 
 	if len(a.Responses) == 0 {
 		return a.Response.SaveToDb2(fmt.Sprintf("user=%s password=%s database=%s table=%s",
-			opts.db2User, opts.db2Password, opts.db2Database, opts.db2Table))
+			opts.Db2User, opts.Db2Password, opts.Db2Database, opts.Db2Table))
 	} else if len(a.Responses) > 1 {
 		for _, ar := range a.Responses {
 			err := ar.SaveToDb2(fmt.Sprintf("user=%s password=%s database=%s table=%s",
-				opts.db2User, opts.db2Password, opts.db2Database, opts.db2Table))
+				opts.Db2User, opts.Db2Password, opts.Db2Database, opts.Db2Table))
 			if err != nil {
 				return err
 			}
@@ -176,36 +185,64 @@ func SaveToDb2(a *goapod.Apod, opts *Options) error {
 }
 
 func main() {
-	var opts *Options
-	var err error
-	var saveToYaml, readFromYaml bool
-	var yamlFile string
-
-	// First, check if we should read from YAML
-	// We need to do a preliminary parse to check for read-yaml flag
-	flag.BoolVar(&readFromYaml, "read-yaml", false, "read options from YAML file")
-	flag.StringVar(&yamlFile, "yaml-file", "apod_options.yaml", "YAML file path for saving/reading options")
 	flag.Parse()
 
+	if help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	var err error
+
 	if readFromYaml {
-		opts, err = SetOptionsFromFile(yamlFile)
+		optsFromFile, err := SetOptionsFromFile(yamlFile)
 		if err != nil {
 			fmt.Printf("Error reading from YAML file: %v\n", err)
 			os.Exit(1)
 		}
-	} else {
-		// Reset flag parsing and use SetOptionsWithYamlFlags
-		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-		var saveToYamlPtr, readFromYamlPtr *bool
-		var yamlFilePtr *string
-		opts, saveToYamlPtr, readFromYamlPtr, yamlFilePtr, err = SetOptionsWithYamlFlags()
+		opts = *optsFromFile
+	}
+
+	a := goapod.Apod{}
+
+	if opts.Date != "" {
+		err := VerifyDate(opts.Date)
 		if err != nil {
-			fmt.Printf("Error setting options: %v\n", err)
+			fmt.Println(err)
 			os.Exit(1)
 		}
-		saveToYaml = *saveToYamlPtr
-		readFromYaml = *readFromYamlPtr
-		yamlFile = *yamlFilePtr
+		a.Date = opts.Date
+	} else if opts.Startdate != "" && opts.Enddate != "" {
+		err := VerifyDate(opts.Startdate)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		a.StartDate = opts.Startdate
+
+		err = VerifyDate(opts.Enddate)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		a.EndDate = opts.Enddate
+	}
+
+	err = a.Fetch()
+	if err != nil {
+		fmt.Printf("Error fetching APOD: %v\n", err)
+		os.Exit(1)
+	}
+
+	PrintApod(&a)
+
+	// download image
+	if opts.Download {
+		err = SaveImage(&a, opts.DstFile, opts.Hdurl)
+		if err != nil {
+			fmt.Printf("Error downloading image: %v\n", err)
+			os.Exit(1)
+		}
 	}
 
 	// Save to YAML if requested
@@ -218,55 +255,14 @@ func main() {
 		fmt.Printf("Options saved to %s\n", yamlFile)
 	}
 
-	a := goapod.Apod{}
-
-	if opts.date != "" {
-		err := VerifyDate(opts.date)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		a.Date = opts.date
-	} else if opts.startdate != "" && opts.enddate != "" {
-		err := VerifyDate(opts.startdate)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		a.StartDate = opts.startdate
-
-		err = VerifyDate(opts.enddate)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		a.EndDate = opts.enddate
-	}
-
-	err = a.Fetch()
-	if err != nil {
-		fmt.Printf("Error fetching APOD: %v\n", err)
-		os.Exit(1)
-	}
-
-	PrintApod(&a)
-
-	// download image
-	if opts.download {
-		err = SaveImage(&a, opts.dstFile, opts.hdurl)
-		if err != nil {
-			fmt.Printf("Error downloading image: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
 	// save to DB2
-	if opts.db2 {
-		err := SaveToDb2(&a, opts)
+	if opts.Db2 {
+		err := SaveToDb2(&a, &opts)
 		if err != nil {
 			fmt.Println("Error saving to DB2:", err)
 			os.Exit(1)
 		}
 		fmt.Println("APOD saved to DB2 successfully.")
 	}
+
 }
